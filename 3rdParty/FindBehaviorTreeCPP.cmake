20d91156e78118d8ac3cdf36ddb57fdaaf11a2ae
Axel Nana <axel.nana@aliens-group.com>
2021-07-13 00:09:12 +0100

    ➕ Add BehaviorTree.CPP 3.5.6 as a dependency

--- 3rdParty/FindBehaviorTreeCPP.cmake
+++ 3rdParty/FindBehaviorTreeCPP.cmake
@@ -7,7 +7,7 @@
 
 ly_add_external_target(
     NAME BehaviorTreeCPP
-    VERSION 3.3.0
+    VERSION 3.5.6
     3RDPARTY_ROOT_DIRECTORY ${o3de_gem_path}/External/BehaviorTree.CPP
     INCLUDE_DIRECTORIES include
 )
---
+++ 3rdParty/Platform/Windows/BehaviorTreeCPP_windows.cmake
@@ -0,0 +1,5 @@
+if (CMAKE_BUILD_TYPE EQUAL "release")
+    set(BEHAVIORTREECPP_LIBS ${BASE_PATH}/win/lib/behaviortree_cpp_v3.lib)
+else()
+    set(BEHAVIORTREECPP_LIBS ${BASE_PATH}/win/lib/behaviortree_cpp_v3d.lib)
+endif()
