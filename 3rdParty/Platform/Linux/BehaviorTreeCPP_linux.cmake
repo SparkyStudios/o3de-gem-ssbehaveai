e589df2b105ecffa3c0b1a3847f68630197b9d0e
Axel Nana <axel.nana@aliens-group.com>
2021-07-14 00:45:14 +0100

    Profile builds will use the release build of BehaviorTree.CPP

--- 3rdParty/Platform/Linux/BehaviorTreeCPP_linux.cmake
+++ 3rdParty/Platform/Linux/BehaviorTreeCPP_linux.cmake
@@ -1,5 +1,5 @@
-if (CMAKE_BUILD_TYPE EQUAL "release")
-    set(BEHAVIORTREECPP_LIBS ${BASE_PATH}/linux/lib/behaviortree_cpp_v3.lib)
-else()
+if (CMAKE_BUILD_TYPE EQUAL "debug")
     set(BEHAVIORTREECPP_LIBS ${BASE_PATH}/linux/lib/behaviortree_cpp_v3d.lib)
+else()
+    set(BEHAVIORTREECPP_LIBS ${BASE_PATH}/linux/lib/behaviortree_cpp_v3.lib)
 endif()
