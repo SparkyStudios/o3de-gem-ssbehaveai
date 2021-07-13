288bd4846d530f51d0b8063dde319bbb66c1bef7
Axel Nana <axel.nana@aliens-group.com>
2021-07-13 23:10:47 +0100

    Register all new files to the CMake project

--- Code/ssbehaviortree_files.cmake
+++ Code/ssbehaviortree_files.cmake
@@ -1,6 +1,19 @@
 
 set(FILES
     Include/SparkyStudios/AI/BehaviorTree/SSBehaviorTreeBus.h
+
+    Include/SparkyStudios/AI/BehaviorTree/Blackboard/SSBehaviorTreeBlackboardProperty.h
+
+    Include/SparkyStudios/AI/BehaviorTree/Core/SSBehaviorTreeFactory.h
+    Include/SparkyStudios/AI/BehaviorTree/Core/SSBehaviorTreeRegistry.h
+
+    Include/SparkyStudios/AI/BehaviorTree/Nodes/SSBehaviorTreeNode.h
+
+    Source/Blackboard/SSBehaviorTreeBlackboardProperty.cpp
+
+    Source/Core/SSBehaviorTreeFactory.cpp
+    Source/Core/SSBehaviorTreeRegistry.cpp
+
     Source/SSBehaviorTreeModuleInterface.h
     Source/SSBehaviorTreeSystemComponent.cpp
     Source/SSBehaviorTreeSystemComponent.h
