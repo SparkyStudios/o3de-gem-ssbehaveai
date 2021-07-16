07fd1a20e36f6edbdefd1091317a80c6ebd3a5c3
Axel Nana <axel.nana@aliens-group.com>
2021-07-16 05:15:12 +0100

    ✨ SimpleMotionGetBlendInTimeNode

---
+++ Code/Include/SparkyStudios/AI/BehaviorTree/Nodes/Animation/SimpleMotionGetBlendInTimeNode.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SparkyStudios/AI/BehaviorTree/Core/SSBehaviorTreeNode.h>
+
+#include <Integration/SimpleMotionComponentBus.h>
+
+namespace SparkyStudios::AI::BehaviorTree::Nodes::Animation
+{
+    class SimpleMotionGetBlendInTimeNode : public Core::SSBehaviorTreeNode
+    {
+    public:
+        AZ_CLASS_ALLOCATOR(SimpleMotionGetBlendInTimeNode, AZ::SystemAllocator, 0);
+        AZ_RTTI(SimpleMotionGetBlendInTimeNode, "{9df18a09-5b84-4f63-9cbf-a3350ae17773}", Core::SSBehaviorTreeNode);
+
+        static constexpr const char* NODE_NAME = "SimpleMotionGetBlendInTime";
+
+        static constexpr const char* NODE_PORT_VALUE_NAME = "value";
+        static constexpr const char* NODE_PORT_VALUE_DESCRIPTION = "The blackboard entry in which the value should be set.";
+
+        SimpleMotionGetBlendInTimeNode(const std::string& name, const Core::SSBehaviorTreeNodeConfiguration& config);
+
+        static void Reflect(AZ::ReflectContext* context);
+
+        static void RegisterNode(const AZStd::shared_ptr<Core::SSBehaviorTreeRegistry>& registry);
+
+        static Core::SSBehaviorTreePortsList providedPorts();
+
+    protected:
+        Core::SSBehaviorTreeNodeStatus Tick();
+    };
+} // namespace SparkyStudios::AI::BehaviorTree::Nodes::Animation
---
+++ Code/Source/Nodes/Animation/SimpleMotionGetBlendInTimeNode.cpp
@@ -0,0 +1,40 @@
+#include <StdAfx.h>
+
+#include <SparkyStudios/AI/BehaviorTree/Nodes/Animation/SimpleMotionGetBlendInTimeNode.h>
+
+namespace SparkyStudios::AI::BehaviorTree::Nodes::Animation
+{
+    SimpleMotionGetBlendInTimeNode::SimpleMotionGetBlendInTimeNode(
+        const std::string& name, const Core::SSBehaviorTreeNodeConfiguration& config)
+        : SSBehaviorTreeNodeBase(name, config)
+    {
+    }
+
+    void SimpleMotionGetBlendInTimeNode::Reflect(AZ::ReflectContext* context)
+    {
+        AZ_UNUSED(context);
+    }
+
+    void SimpleMotionGetBlendInTimeNode::RegisterNode(const AZStd::shared_ptr<Core::SSBehaviorTreeRegistry>& registry);
+    {
+        registry->DelayNodeRegistration(NODE_NAME);
+    }
+
+    Core::SSBehaviorTreePortsList SimpleMotionGetBlendInTimeNode::providedPorts()
+    {
+        Core::SSBehaviorTreePortsList ports = Core::SSBehaviorTreeNode::providedPorts();
+
+        ports.merge(Core::SSBehaviorTreePortsList({ BT::OutputPort<float>(NODE_PORT_VALUE_NAME, NODE_PORT_VALUE_DESCRIPTION) }));
+
+        return ports;
+    }
+
+    Core::SSBehaviorTreeNodeStatus SimpleMotionGetBlendInTimeNode::Tick()
+    {
+        float value;
+        EBUS_EVENT_ID_RESULT(value, GetEntityId(), EMotionFX::Integration::SimpleMotionComponentRequestBus, GetBlendInTime);
+        SetOutputValue<float>(NODE_PORT_VALUE_NAME, value);
+
+        return Core::SSBehaviorTreeNodeStatus::SUCCESS;
+    }
+} // namespace SparkyStudios::AI::BehaviorTree::Nodes::Animation
