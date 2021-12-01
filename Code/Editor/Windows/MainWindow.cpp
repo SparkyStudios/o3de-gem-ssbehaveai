7ac6dbf1a10f976019d12481e43780b1b8dccff1
Axel Nana <axel.nana@aliens-group.com>
2021-12-01 15:04:56 +0100

    Add Apache-2.0 License

--- Code/Editor/Windows/MainWindow.cpp
+++ Code/Editor/Windows/MainWindow.cpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2021-present Sparky Studios. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <Editor/Application/precompiled.h>
 
 #include <Editor/Core/EditorBase.h>
@@ -32,6 +46,7 @@ namespace SparkyStudios::AI::BehaviorTree::Editor::Windows
     MainWindow::MainWindow(QWidget* parent, const AZ::IO::PathView& projectPath, const AZ::IO::PathView& filePath)
         : AzQtComponents::DockMainWindow(parent)
         , _current_layout(QtNodes::PortLayout::Vertical)
+        , _dirty_file(false)
     {
         setWindowTitle(tr("BehaviorTree Editor"));
         setMinimumSize(800, 600);
