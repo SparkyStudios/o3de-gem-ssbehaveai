4a195a8ce26ccd0a6f484b87c1d2b1f69589fd97
Axel Nana <axel.nana@aliens-group.com>
2022-03-14 02:21:51 +0100

    Use new `o3de_pal_dir` CMake function

--- 3rdParty/FindBehaviorTreeCPP.cmake
+++ 3rdParty/FindBehaviorTreeCPP.cmake
@@ -2,6 +2,6 @@
 ly_add_external_target(
     NAME BehaviorTreeCPP
     VERSION 3.5.6
-    3RDPARTY_ROOT_DIRECTORY ${o3de_gem_path}/External/BehaviorTree.CPP
+    3RDPARTY_ROOT_DIRECTORY ${gem_path}/External/BehaviorTree.CPP
     INCLUDE_DIRECTORIES include
 )
--- 3rdParty/FindQtNodeEditor.cmake
+++ 3rdParty/FindQtNodeEditor.cmake
@@ -2,6 +2,6 @@
 ly_add_external_target(
     NAME QtNodeEditor
     VERSION 2.1.3
-    3RDPARTY_ROOT_DIRECTORY ${o3de_gem_path}/External/QtNodeEditor
+    3RDPARTY_ROOT_DIRECTORY ${gem_path}/External/QtNodeEditor
     INCLUDE_DIRECTORIES include
 )
