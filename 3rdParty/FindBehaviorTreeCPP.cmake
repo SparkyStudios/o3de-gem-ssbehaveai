7d686f903a5779db8665fb278da3bf37cecd35a0
Axel Nana <axel.nana@aliens-group.com>
2022-03-20 01:59:53 +0100

    feat: :arrow_up: Upgrade BehaviorTree.CPP dependency

--- 3rdParty/FindBehaviorTreeCPP.cmake
+++ 3rdParty/FindBehaviorTreeCPP.cmake
@@ -1,7 +1,7 @@
 
 ly_add_external_target(
     NAME BehaviorTreeCPP
-    VERSION 3.5.6
+    VERSION 3.6.1
     3RDPARTY_ROOT_DIRECTORY ${gem_path}/External/BehaviorTree.CPP
     INCLUDE_DIRECTORIES include
 )
