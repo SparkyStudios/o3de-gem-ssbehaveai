24622fe6d75c3da9c68014b24f95bb02aaa44681
Axel Nana <axel.nana@aliens-group.com>
2022-03-20 02:04:11 +0100

    Update 3rdParty cmake files

--- 3rdParty/Platform/Linux/QtNodeEditor_linux.cmake
+++ 3rdParty/Platform/Linux/QtNodeEditor_linux.cmake
@@ -1,5 +1,15 @@
-if (CMAKE_BUILD_TYPE EQUAL "debug")
-    set(QTNODEEDITOR_LIBS ${BASE_PATH}/lib/linux/nodes_d.lib)
-else()
-    set(QTNODEEDITOR_LIBS ${BASE_PATH}/lib/linux/nodes.lib)
-endif()
+# Copyright (c) 2021-present Sparky Studios. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set(QTNODEEDITOR_LIBS ${BASE_PATH}/lib/linux/$<IF:$<CONFIG:Debug>,nodes_d.lib,nodes.lib>)
--- 3rdParty/Platform/Windows/BehaviorTreeCPP_windows.cmake
+++ 3rdParty/Platform/Windows/BehaviorTreeCPP_windows.cmake
@@ -1,1 +1,15 @@
-set(BEHAVIORTREECPP_LIBS $<IF:$<CONFIG:Debug>,${BASE_PATH}/lib/win/behaviortree_cpp_v3d.lib,${BASE_PATH}/lib/win/behaviortree_cpp_v3.lib>)
+# Copyright (c) 2021-present Sparky Studios. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set(BEHAVIORTREECPP_LIBS ${BASE_PATH}/lib/win/$<IF:$<CONFIG:Debug>,behaviortree_cpp_v3d.lib,behaviortree_cpp_v3.lib>)
